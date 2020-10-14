31138208ac049f762cc61b6be0e7a3eb507d36f0
Lajos Nagy <lajos.j.nagy@gmail.com>
2020-10-14 21:41:28 +0000

    main.py pakli_lapjai (nagy batük lecserélése)

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import random
 
 #   váltózok
 
-pakli_lapjai = ['kör 2', 'kör 3', 'kör 4', 'kör 5', 'kör 6', 'kör 7', 'kör 8', 'kör 9', 'kör 10', 'kör J', 'kör Q', 'kör K', 'kör A',
-'káró 2', 'káró 3', 'káró 4', 'káró 5', 'káró 6', 'káró 7', 'káró 8 ', 'káró 9', 'káró 10', 'káró J', 'káró Q', 'káró K', 'káró A',
-'treff 2', 'treff 3', 'treff 4', 'treff 5', 'treff 6', 'treff 7', 'treff 8', 'treff 9', 'treff 10', 'treff J', 'treff Q', 'treff K', 'treff A',
-'pikk 2', 'pikk 3', 'pikk 4', 'pikk 5', 'pikk 6', 'pikk 7', 'pikk 8', 'pikk 9', 'pikk 10', 'pikk J', 'pikk Q', 'pikk K', 'pikk A']
+pakli_lapjai = ['kör 2', 'kör 3', 'kör 4', 'kör 5', 'kör 6', 'kör 7', 'kör 8', 'kör 9', 'kör 10', 'kör j', 'kör q', 'kör k', 'kör a',
+'káró 2', 'káró 3', 'káró 4', 'káró 5', 'káró 6', 'káró 7', 'káró 8 ', 'káró 9', 'káró 10', 'káró j', 'káró q', 'káró k', 'káró a',
+'treff 2', 'treff 3', 'treff 4', 'treff 5', 'treff 6', 'treff 7', 'treff 8', 'treff 9', 'treff 10', 'treff j', 'treff q', 'treff k', 'treff a',
+'pikk 2', 'pikk 3', 'pikk 4', 'pikk 5', 'pikk 6', 'pikk 7', 'pikk 8', 'pikk 9', 'pikk 10', 'pikk j', 'pikk q', 'pikk k', 'pikk a']
 
 # pakli_szinei = ['kör', 'káró', 'treff', 'pikk']
 
@@ -23,7 +23,7 @@ figurak = ["J", "Q", "K"]
 def lap_huzas(kihuzott_lap):
     lap = random.choice(pakli_lapjai)
     i = random.choice(pakli_lapjai)
-    del pakli_lapjai[i]
+    #del pakli_lapjai[i]
     return lap
     
 
