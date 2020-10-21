67b866caa4d189220cbf1a61339cacced4be964f
Lajos <lajos.j.nagy@gmail.com>
2020-10-21 13:24:18 +0200

    kihúzott lap törlése a pakliból (for ciklussal)

--- teszt.py
+++ teszt.py
@@ -1,9 +1,18 @@
+#   imprortált modulok
+
 import random
+
+#   globális változók
 
 a = []
 b = ['kör', 'káró', 'treff', 'pikk']
 c = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
+lap = 0
+index = None
+
+
+#   függvények / metodusok
 
 def pakli_osszeall():
     for szin in b:
@@ -11,17 +20,31 @@ def pakli_osszeall():
             kozos = szin, szam
             a.append(kozos)
 
+def lap_huzas(kihuzott_lap):
+    lap = random.choice(a)
+    print(lap)
+    return lap
         
+# def kihuzott_lap_indexe(lap):
+#     index = a.index(lap)
+#     return index
 
-# def lap_huzas(p):
-#     return random.choice(a)
-
+# print(kihuzott_lap_indexe(lap))
 
 pakli_osszeall()
-# print(lap_huzas(a))
 
+
+for i in range(len(a)-1):
+    a.remove(lap_huzas(a))
+    print(len(a))
+    # if len(a) <= 0:
+    #     break
+
+# pakli lapjainak számolása
 print(len(a))
-print(a[0])
+print(lap_huzas(lap))
+
+# print(lap)
 
 
 
