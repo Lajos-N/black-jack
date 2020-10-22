e0b61f525c251cbe415f9449c90da19aa68bd092
Lajos Nagy <lajos.j.nagy@gmail.com>
2020-10-22 14:59:15 +0000

    teszt.py lap eltávolító függvény hozzáadása

--- teszt.py
+++ teszt.py
@@ -9,29 +9,37 @@ pakli_szin = ['kör', 'káró', 'treff', 'pikk']
 pakli_szam = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 lap = 0
-index = None
+
 
 
 #   függvények / metodusok
 
+# összeállítja a paklit
 def pakli_osszeall():
     for szin in pakli_szin:
         for szam in pakli_szam:     
             kozos = szin, szam
             pakli.append(kozos)
 
+# a pakliból kihúz egy lapot amit átad a lap változónak
 def lap_huzas(kihuzott_lap):
     lap = random.choice(pakli)
-    print(lap)
+    #print(lap)
     return lap
+	
+def lap_eltavolito(lap):
+	pakli.remove(lap_huzas(pakli))
+	
+
+# for ciklussal kihúzott lapok törlése a pakliból
+def pakli_tesztelö_ciklus():
+	for i in range(len(pakli)-1):
+		pakli.remove(lap_huzas(pakli))
+		print(len(pakli))
 
 
 pakli_osszeall()
-
-
-for i in range(len(pakli)-1):
-    pakli.remove(lap_huzas(pakli))
-    print(len(pakli))
+#pakli_tesztelö_ciklus()
 
 
 #	föprogram
@@ -39,6 +47,25 @@ for i in range(len(pakli)-1):
 # pakli lapjainak számolása
 print(len(pakli))
 print(lap_huzas(lap))
+lap_eltavolito(lap)
+print()
+
+
+print(len(pakli))
+print(lap_huzas(lap))
+lap_eltavolito(lap)
+print()
+
+print(len(pakli))
+print(lap_huzas(lap))
+lap_eltavolito(lap)
+print()
+
+print()
+print()
+print(len(pakli))
+print(pakli)
+
 
 
 
